4916e70c7211605121945800bc78909af1509167
oldprinters <zakaz@oldprinters.ru>
2024-02-29 22:52:02 +0300

    m0

--- src/main.cpp
+++ src/main.cpp
@@ -10,11 +10,48 @@ NTPClient timeClient(ntpUDP, "pool.ntp.org", 10800, 3600000);
 int tftCS = 5; // screen chip select pin
 int tftDC = 26; // data command select pin
 int tftRST = 25; // reset pin
-
+class MTime{
+    int16_t h;
+    int16_t m;
+    int16_t s;
+    public:
+    MTime(){
+        h = 0;
+        m = 0;
+        s = 0;
+    };
+    //-----------------------
+    int16_t compare(NTPClient *tC){
+        int16_t th = tC->getHours();
+        int16_t tm = tC->getMinutes();
+        int16_t ts = tC->getSeconds();
+        int16_t rez = 0;
+        if(th != h){
+            rez = 7;
+        }else if(tm != m) {
+            rez = 3;
+        }else if(ts != s){
+            rez = 1;
+        }
+        return rez;
+    }
+    //---------------------------
+    void set(NTPClient *tC){
+        h = tC->getHours();
+        m = tC->getMinutes();
+        s = tC->getSeconds();
+    }
+};
+MTime oldTime;
+//---------------------------------------------
 const char* ssid = "ivanych";
 const char* password = "stroykomitet";
 WiFiClient client;
-
+//---------------------------------------------
+//положение часов
+int cursPosX = 30;
+int cursPosY = 10;
+//---------------------------------------------
 // associate tft with ILI9341 lib
 Adafruit_ILI9341 tft = Adafruit_ILI9341(tftCS, tftDC, tftRST);
 String texts[] = // color names
@@ -27,30 +64,44 @@ String text;
 unsigned int color, chkTime;
 void setup()
 {
+    tft.begin(); // initialise screen
+    tft.setRotation(1); // portrait, connections at top
+    // tft.fillScreen(ILI9341_BLACK);
+    tft.fillRect(0,0, 319, 239, ILI9341_BLACK);
+
+    // tft.fillScreen(ILI9341_BLACK); // fill screen in black
     Serial.begin(115200); // Serial Monitor baud rate
     WiFi.begin(ssid, password); // initialise Wi-Fi and wait
-    while (WiFi.status() != WL_CONNECTED) delay(500);
-    // for Wi-Fi connection
-    Serial.print("IP address: ");
-    Serial.println(WiFi.localIP()); // Wi-Fi network IP address
-    Serial.println("Connecting...");
-
+    tft.print("Connecting...");
+    while (WiFi.status() != WL_CONNECTED){
+        tft.print(".");
+        delay(500);
+    }
     timeClient.begin();
     timeClient.update();
 
-    tft.begin(); // initialise screen
-    tft.setRotation(2); // portrait, connections at top
-    tft.fillScreen(ILI9341_BLACK); // fill screen in black
-    tft.drawRect(0,0,239,319,ILI9341_WHITE); // draw white frame line
-    tft.drawRect(1,1,237,317,ILI9341_WHITE); // and second frame line
+    tft.drawRect(0,0,319,239,ILI9341_BLUE); // draw white frame line
+    tft.drawRect(1,1,237,317,ILI9341_RED); // and second frame line
     tft.setTextSize(4); // set text size
 }
+//------------------------------------------------------------------------
+void outTime(NTPClient *tk){
+    tft.setCursor(cursPosX, cursPosY);
+    tk->update();
+    tft.setTextSize(4); // set text size
+    int16_t c = oldTime.compare(tk);
+    tft.fillRect(cursPosX + (3 - c) * 60, cursPosY ,cursPosX + 180,cursPosY + 150,ILI9341_BLACK);
+    tft.println(tk->getFormattedTime());
+    tft.setTextSize(2); // set text size
+    tft.print("          ");
+    tft.print(oldTime.compare(tk));
+    oldTime.set(tk);
+}
+//------------------------------------------------------------------------
 void loop()
 {
-    tft.setCursor(15, 25);
-    timeClient.update();
-    tft.fillRect(15,15,200,100,ILI9341_BLACK);
-    tft.print(timeClient.getFormattedTime());
+    // tft.print(timeClient.getFormattedTime());
+    outTime(&timeClient);
     delay(1000);
     //  // clear screen apart from frame
     // tft.fillRect(2,2,235,314,ILI9341_BLACK);
